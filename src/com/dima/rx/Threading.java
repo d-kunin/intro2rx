b60db44884a7faff64c1f73fa38b8f95c5acd066
Dima Kunin <mr.kunin@gmail.com>
2016-06-16 18:17:43 +0200

    AsyncUtil examples.

--- src/com/dima/rx/Threading.java
+++ src/com/dima/rx/Threading.java
@@ -1,9 +1,13 @@
 package com.dima.rx;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.functions.Action1;
+import rx.functions.Action2;
 import rx.schedulers.Schedulers;
+import rx.util.async.Async;
 
 /**
  * Created by dima on 6/16/16.
@@ -14,7 +18,7 @@ public class Threading implements Main.Snippet {
     return Observable.create(new Observable.OnSubscribe<String>() {
       @Override
       public void call(Subscriber<? super String> subscriber) {
-        String s = String.format("Produced on: %s", Thread.currentThread().getName());
+        String s = addThreadName("Produced on: %s");
         subscriber.onNext(s);
         subscriber.onCompleted();
       }
@@ -22,10 +26,14 @@ public class Threading implements Main.Snippet {
   }
 
   final Action1<String> action = production -> {
-    String consumption = String.format("Consumed on: %s", Thread.currentThread().getName());
+    String consumption = addThreadName("Consumed on: %s");
     System.out.println(production);
     System.out.println(consumption);
   };
+
+  private static String addThreadName(String format) {
+    return String.format(format, Thread.currentThread().getName());
+  }
 
   @Override
   public void run() {
@@ -43,6 +51,26 @@ public class Threading implements Main.Snippet {
     create()
         .subscribeOn(Schedulers.computation())
         .observeOn(Schedulers.io())
+        .subscribe(action);
+
+
+    Async.start(() -> addThreadName("start() Asynchronously produced on: %s"))
+            .subscribe(action);
+
+    Async.runAsync(
+            Schedulers.newThread(),
+            new Action2<Observer<? super String>, Subscription>() {
+              @Override
+              public void call(Observer<? super String> observer, Subscription subscription) {
+                observer.onNext(addThreadName("runAsync() produced %s"));
+                observer.onCompleted();
+              }
+            })
+            .subscribe(action);
+
+    Async
+        .fromAction(
+            () -> { action.call(addThreadName("fromAction() %s")); }, "fromAction() result", Schedulers.newThread())
         .subscribe(action);
 
     try {
